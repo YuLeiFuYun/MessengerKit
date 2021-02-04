99bb7fa8f37d08dac329741e5fac1084d8c0ea7b
玉垒浮云 <yuleifuyunn@gmail.com>
2021-02-04 15:03:53 +0800

    fix some bugs

--- MessengerKit/Collection View/Cells/MSGMessageCell.swift
+++ MessengerKit/Collection View/Cells/MSGMessageCell.swift
@@ -105,3 +105,15 @@ extension MSGMessageCell {
         delegate?.cellTapReceived(for: message)
     }
 }
+
+extension UIApplication {
+    /// Checks if view hierarchy of application contains `UIRemoteKeyboardWindow` if it does, keyboard is presented
+    var isKeyboardPresented: Bool {
+        if let keyboardWindowClass = NSClassFromString("UIRemoteKeyboardWindow"),
+            self.windows.contains(where: { $0.isKind(of: keyboardWindowClass) }) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
